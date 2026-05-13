881573972a4b161f2740fa46669211c6d26ddd1f
Uzair Ahmad <154722187+uzairatta@users.noreply.github.com>
2026-05-13 14:00:15 +0500

    Update jwt.js

--- Utils/jwt.js
+++ Utils/jwt.js
@@ -1,21 +1,13 @@
 import jwt from "jsonwebtoken";
 
 export const signJWT = (payload) => {
-    try {
-        const token = jwt.sign(payload, process.env.JWT_SECRET, {
-            expiresIn: "1h",
-        });
-        return token;
-    } catch (error) {
-        throw new Error("Error signing JWT");
-    }
+    const token = jwt.sign(payload, process.env.JWT_SECRET, {
+        expiresIn: "1h",
+    });
+    return token;
 };
 
 export const verifyJWT = (token) => {
-    try {
-        const decoded = jwt.verify(token, process.env.JWT_SECRET);
-        return decoded;
-    } catch (error) {
-        throw new Error("Error verifying JWT");
-    }
+    const decoded = jwt.verify(token, process.env.JWT_SECRET);
+    return decoded;
 };
